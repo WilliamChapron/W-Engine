a8db50fbfcd396391ecca98702430efe1ec01a39
William <williamchapronpro@gmail.com>
2024-10-24 18:57:01 +0200

    Training Tests on render triangle, render rectangle etc
    Training Tests on render triangle, render rectangle etc

--- pch.h
+++ pch.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#pragma comment(lib, "d3d12.lib")
-#pragma comment(lib, "dxgi.lib")
-#pragma comment(lib, "d3dcompiler.lib")
+//#pragma comment(lib, "d3d12.lib")
+//#pragma comment(lib, "dxgi.lib")
+//#pragma comment(lib, "d3dcompiler.lib")
 
 #include <windows.h>
 
@@ -29,14 +29,21 @@
 #include <iostream>
 
 
-#include <wrl.h>
-#include <dxgi1_4.h>
-#include <d3d12.h>
-#include <d3d11_1.h>
-#include <D3Dcompiler.h>
-#include <DirectXMath.h>
-#include <DirectXPackedVector.h>
-#include <DirectXColors.h>
-#include <DirectXCollision.h>
-#include <wrl/client.h>
-#include <WindowsX.h>
+//#include <wrl.h>
+//#include <dxgi1_4.h>
+//#include <d3d12.h>
+//#include <d3d11_1.h>
+//#include <D3Dcompiler.h>
+//#include <DirectXMath.h>
+//#include <DirectXPackedVector.h>
+//#include <DirectXColors.h>
+//#include <DirectXCollision.h>
+//#include <wrl/client.h>
+//#include <WindowsX.h>
+
+
+#include <glad/glad.h>  
+#include <GLFW/glfw3.h> 
+
+#pragma comment(lib, "glfw3.lib")
+#pragma comment(lib, "opengl32.lib") // OpenGL est souvent nécessaire pour GLFW
